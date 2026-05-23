760d60fbe9cac81c85ef7a2980a65af5479efed5
fremag <fremag.gh@gmail.com>
2026-05-24 00:54:22 +0200

    Chapter 1: lexer (add --lex option so test for chap1/lex is ok)

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ fn run_preprocessor(gcc_path: &str, input_file: &str, output_file: &Path) -> std
 
 fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
-    let input_file = &args[1];
+    let action = &args[1];
+    let input_file = &args[2];
 
     let output_file = compute_output_file(input_file);
     let output_file_str = output_file.to_str().unwrap();
@@ -34,21 +35,26 @@ fn main() -> Result<(), std::io::Error> {
     println!("{output}");
 
     let contents = fs::read_to_string(&output_file)
-         .expect("Should have been able to read the file");
+         .expect(&format!("Should have been able to read the file {}", &output_file_str));
     let lexer  = Lexer::new(contents);
     let tokens = match lexer.tokenize()  {
         Err(err_msg) => {
             print!("{err_msg}");
-            process::exit(1); 
+            process::exit(1);
         }
         Ok(tokens) => tokens
     };
-    
+
     println!("Tokens:");
     for token in tokens {
         println!("{token}");
     }
 
+    if action == "--lex" {
+        // we only want to lex so let's exit here  
+        process::exit(0);
+    }
+    
     println!("Done.");
     process::exit(0);
 }
